60b90e35d298a25e2c9dad628c303a396e45b79d
Gustavo <gustavo.com>
2015-09-12 10:39:11 -0300

    Definição da Pagina inicial e de contato do Projeto

---
+++ contato.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Pagina do Contato</title>
+</head>
+<body>
+	<h1>Pagina do Contato</h1>
+	<a href=index.php>Pagina inicial</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Pagina Inicial do Projeto</title>
+</head>
+<body>
+	<h1>Pagina Inicial</h1>h1>
+	<a href="contato.php">Ir para contato</a>a>
+</body>
+</html>
