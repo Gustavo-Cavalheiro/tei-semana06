7637e88be5083b095ed0cdb491ea171ee2443025
Gustavo <gustavo.com>
2015-09-12 10:44:19 -0300

    Correção

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Pagina Inicial do Projeto</title>
 </head>
 <body>
-	<h1>Pagina Inicial</h1>h1>
-	<a href="contato.php">Ir para contato</a>a>
+	<h1>Pagina Inicial</h1>
+	<a href="contato.php">Ir para contato</a>
 </body>
 </html>
